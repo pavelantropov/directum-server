1dd45646547866a16f50f4d4f98ba873bd4ab1ee
Pavel Antropov <pavel_antropov@epam.com>
2022-05-14 17:59:53 +0400

    Create an interface and a realization for a data source

---
+++ src/ServerApp/Data/IDataSource.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+namespace ServerApp.Data;
+
+public interface IDataSource
+{
+    string? GetString(string name, CultureInfo culture);
+}
---
+++ src/ServerApp/Data/ResourcesDataSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+using ServerApp.Properties;
+
+namespace ServerApp.Data;
+
+public class ResourcesDataSource : IDataSource
+{
+    public string? GetString(string name, CultureInfo culture) =>
+        Resources.ResourceManager.GetString(name, culture);
+}
